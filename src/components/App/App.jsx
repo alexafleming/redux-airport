b7136c3358c5a20514cb042d6b391ccc8c74b63f
Alexa Fleming <alexaflemingmn@gmail.com>
2023-09-07 16:13:49 -0500

    class activity

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,47 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
 
+  const dispatch = useDispatch()
+  const reduxStore = useSelector(store => store)
+  const [newAirline, setNewAirline] = useState('')
+  const airlinesList = useSelector(store => store.airlinesList)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch({
+      type: 'ADD_AIRLINE',
+      payload: newAirline
+    });
+
+
+    setNewAirline('')
+  }
+
+
+
   return (
     <div>
-      <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
-      <button>Add Airline</button>
-      <table>{/* Airlines should be listed here */}</table>
+      <form onSubmit={handleSubmit}>
+        <h1>Redux Airport</h1>
+        <input
+          type='text'
+          placeholder='Airline Name'
+          value={newAirline}
+          onChange={event => setNewAirline(event.target.value)}
+        />
+        <button>Add Airline</button>
+      </form>
+      <table> 
+        <tbody>
+        {airlinesList.map((airline, i) => (
+        <tr key={i}><td>{airline}</td>
+        </tr>
+      ))}
+      </tbody>
+      </table>
+
     </div>
   );
 }
